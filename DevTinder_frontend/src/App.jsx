6884327d6717de600bab5cffb362fa033437cc83
aryan-098max <aryangupta4279@gmail.com>
2025-10-04 16:05:16 +0530

    Added Routes

--- DevTinder_frontend/src/App.jsx
+++ DevTinder_frontend/src/App.jsx
@@ -1,8 +1,20 @@
+import Body from "./Components/Body"
+import Login from "./Components/Login"
+import Profile from "./Components/Profile"
+import {BrowserRouter, Routes, Route} from 'react-router'
+
 function App() {
 
   return (
    <div>
-      <h1>Hello App</h1>
+      <BrowserRouter basename="/">
+        <Routes>
+           <Route path="/" element={<Body/>}>
+              <Route path="/login" element={<Login/>} />
+              <Route path="/profile" element={<Profile/>} />
+           </Route>
+        </Routes>
+      </BrowserRouter>
    </div>
   )
 }
